0d0c22e18ff8734b0373e1f7630c7a92a8763dd9
Gary Swift <garyswiftmail@gmail.com>
2018-05-14 13:01:17 +0100

    first commit

---
+++ git-merge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo ''
+echo 'How to do a git merge in Vim:'
+echo ''
+echo '1) press "i"'
+echo '2) write your merge message'
+echo '3) press "esc"'
+echo '4) write ":wq"'
+echo '5) then press enter'
+echo ''
+echo 'https://stackoverflow.com/questions/19085807/please-enter-a-commit-message-to-explain-why-this-merge-is-necessary-especially'
